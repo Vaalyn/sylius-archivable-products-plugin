773b30a277872596ecdc4331a972fd292f06bfaa
Vaalyn <vaalyndev@gmail.com>
2020-11-20 03:11:51 +0100

    Prepare plugin skeleton for own code

--- src/DependencyInjection/SyliusArchivableProductsExtension.php
+++ src/DependencyInjection/SyliusArchivableProductsExtension.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin\DependencyInjection;
+namespace VaaChar\SyliusArchivableProductsPlugin\DependencyInjection;
 
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
@@ -10,14 +10,12 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class AcmeSyliusExampleExtension extends Extension
+final class SyliusArchivableProductsExtension extends Extension
 {
     public function load(array $config, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-
-        $loader->load('services.xml');
     }
 
     public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface
